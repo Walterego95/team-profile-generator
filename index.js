c77c110cfe07d04db0ce4a0d6d10b83746ae9eaa
walterego <walter.scimone@gmail.com>
2021-04-23 09:48:50 -0400

    completed Employee class

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const Employee = require('./lib/Employee.js');
 
 let employee = new Employee("walter", "suca@walter.com");
 console.log(employee.getName(), employee.getEmail(), employee.getRole(), employee.getId());
+
+
+const Manager = require('./lib/Manager.js');
+
+let manager = new Manager("big walt", "melasucano@walter.com");
+console.log(manager.getName(), manager.getEmail(), manager.getRole(), manager.getId(), manager.getOfficeNumber());
