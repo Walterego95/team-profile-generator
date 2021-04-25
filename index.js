b285dd4d6de72ab41013e911ccc4e5fa73f2313f
walterego <walter.scimone@gmail.com>
2021-04-25 14:00:36 -0400

    Improvement of the generateHTML printing...

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Internal = require('./lib/Internal.js');
 
+// function to create the team through questions prompt...
 function createTeam(questions, type) {
     return new Promise((resolve, reject) => {
         let team_member = null;
@@ -19,25 +21,25 @@ function createTeam(questions, type) {
             .then(answers => {
                 switch (type) {
                     case "Manager":
-                        team_member = new Manager(answers.name, answers.email, answers.office_number);
+                        team_member = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
                         break;
+
                     case "Engineer":
-                        // TODO change me once class Engineer exists...
-                        team_member = new Manager(answers.name, answers.email, answers.github);
+                        team_member = new Engineer(answers.name, answers.id, answers.email, answers.github);
                         break;
+
                     case "Internal":
-                        // TODO change me once class Internal exists...
-                        team_member = new Manager(answers.name, answers.email, answers.school);
-                        break;                        
+                        team_member = new Internal(answers.name, answers.id, answers.email, answers.school);
+                        break;
                 }
 
-                resolve({team_member: team_member, next: answers.team});
+                resolve({ team_member: team_member, next: answers.team });
             })
             .catch(error => reject(error));
     });
 }
 
-// starting the app, asking about the maanger
+// starting the app, asking about the manager...
 let manager_questions = [
     {
         type: 'input',
@@ -46,21 +48,32 @@ let manager_questions = [
     },
     {
         type: 'input',
+        name: 'id',
+        message: "What's manager's ID?"
+    },
+    {
+        type: 'input',
         name: 'email',
         message: "What's manager's email?"
     },
     {
         type: 'input',
-        name: 'office_number',
-        message: "What's manager's office numbers?"
+        name: 'officeNumber',
+        message: "What's manager's office number?"
     }
 ];
 
+// questions for engineers...
 let engineer_questions = [
     {
         type: 'input',
         name: 'name',
         message: "What's engineer's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What's engineer's ID?"
     },
     {
         type: 'input',
@@ -70,16 +83,21 @@ let engineer_questions = [
     {
         type: 'input',
         name: 'github',
-        message: "What's engineer's github?"
+        message: "What's engineer's github username?"
     }
 ];
 
-
+// questions for interns...
 let internal_questions = [
     {
         type: 'input',
         name: 'name',
         message: "What's internal's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What's internal's ID?"
     },
     {
         type: 'input',
@@ -95,21 +113,22 @@ let internal_questions = [
 
 let team = [];
 
-createTeam(manager_questions, "Manager").then(async(team_response) => {
-    //adding manager into team array...
+createTeam(manager_questions, "Manager").then(async (team_response) => {
+    // adding manager into team array...
     team.push(team_response.team_member);
 
-    while(team_response.next != "Done") {
-        switch(team_response.next) {
+    // asynchronous responses for Intern and Engineer questions...
+    while (team_response.next != "Done") {
+        switch (team_response.next) {
             case "Internal":
                 team_response = await createTeam(internal_questions, "Internal");
                 break;
             case "Engineer":
                 team_response = await createTeam(engineer_questions, "Engineer");
-                break;                
+                break;
         }
 
-        //adding iternal/engineer into team array...
+        // adding internal/engineer into team array...
         team.push(team_response.team_member);
     }
 
@@ -121,25 +140,60 @@ createTeam(manager_questions, "Manager").then(async(team_response) => {
         let team_html = "";
 
         team.forEach(member => {
-            switch(member.constructor.name) {
+            switch (member.constructor.name) {
                 case "Manager":
-                    team_html += "<div class='manager'>Manager Name:" + member.name + "</div>";
+                    team_html += "<div class='card mb-4 box-shadow'>" +
+                        "<div class='card-header'>" +
+                        "<h4 class='my-0 font-weight-normal'>Manager</h4>" +
+                        "</div>" +
+                        "<div class='card-body'>" +
+                        "<h1 class='card-title'>" + member.name + "</h1>" +
+                        "<ul class='list-unstyled mt-3 mb-4'>" +
+                        "<li>Email: " + "<a href=mailto:" + member.email + ">" + member.email + "</a>" + "</li>" +
+                        "<li>Office Number: " + member.officeNumber + "</li>" +
+                        "<li>ID: " + member.id + "</li>" +
+                        "</ul>" +
+                        "</div>";
                     break;
+
                 case "Internal":
-                    team_html += "<div class='internal'>Internal Name:" + member.name + "</div>";
-                    break;  
+                    team_html += "<div class='card mb-4 box-shadow'>" +
+                        "<div class='card-header'>" +
+                        "<h4 class='my-0 font-weight-normal'>Intern</h4>" +
+                        "</div>" +
+                        "<div class='card-body'>" +
+                        "<h1 class='card-title'>" + member.name + "</h1>" +
+                        "<ul class='list-unstyled mt-3 mb-4'>" +
+                        "<li>Email: " + "<a href=mailto:" + member.email + ">" + member.email + "</a>" + "</li>" +
+                        "<li>Graduating from: " + member.school + "</li>" +
+                        "<li>ID: " + member.id + "</li>" +
+                        "</ul>" +
+                        "</div>";
+                    break;
+
                 case "Engineer":
-                    team_html += "<div class='engineer'>Engineer Name:" + member.name + "</div>";
-                    break;                                
+                    team_html += "<div class='card mb-4 box-shadow'>" +
+                        "<div class='card-header'>" +
+                        "<h4 class='my-0 font-weight-normal'>Engineer</h4>" +
+                        "</div>" +
+                        "<div class='card-body'>" +
+                        "<h1 class='card-title'>" + member.name + "</h1>" +
+                        "<ul class='list-unstyled mt-3 mb-4'>" +
+                        "<li>Email: " + "<a href=mailto:" + member.email + ">" + member.email + "</a>" + "</li>" +
+                        "<li>Github Username: " + "<a href='https://www.github.com/" + member.github + "' " + "target='_blank' " + "rel='noopener noreferrer'" + ">" + member.github + "</a>" + "</li>" +
+                        "<li>ID: " + member.id + "</li>" +
+                        "</ul>" +
+                        "</div>";
+                    break;
             }
-        });
             console.log(team_html);
+        });
         fs.writeFile('./index.html', original_html.replace("REPLACE_ME", team_html), error => {
             if (error) {
                 console.error(error);
                 return false;
             }
-        
+
             console.log("html is ready.");
         });
     });
